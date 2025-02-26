88bc872519086550e661aaadc83b14ae640b5ca6
DEV-NG <nwachukujosh2000@gmail.com>
2025-02-26 11:41:22 +0100

    main

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "like this video",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "subdcribe to support the team",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "leave a comment",
+        completed: true,
+    },
+]
