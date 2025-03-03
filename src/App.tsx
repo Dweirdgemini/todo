739ba60c24665ddf289e206e78ba86c1c29f22de
DEV-NG <nwachukujosh2000@gmail.com>
2025-03-03 23:10:42 +0100

    made dure delete all todos function working

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,9 @@ function App() {
   function deleteTodo(id:number) {
     setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
   }
+  function deleteAllCompletedTodos() {
+    setTodos(prevTodos => prevTodos.filter(todo => !todo.completed));
+  }
 
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
@@ -44,7 +47,7 @@ function App() {
     </div>
     <TodoSummary
     todos={todos}
-    deleteAllCompleted={() => {}}
+    deleteAllCompleted={deleteAllCompletedTodos}
     />
     </main>
   );
--- src/components/TodoSummary.tsx
+++ src/components/TodoSummary.tsx
@@ -24,6 +24,6 @@ export default function TodoSummaryProps({
                 </button>
             )}
         </div>
-    )
+    );
 
 }
